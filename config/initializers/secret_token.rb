5f00777aac256fc7ab92b6c475bd9445c82e45b1
Omar <oldfashionhardcore@gmail.com>
2010-11-11 09:39:02 -0800

    First sample_app_ch3 commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleAppCh3::Application.config.secret_token = 'f49d1bd4a07d077e415035c05f6385f4ca7655a60e551f292661ae581f748383fdb503737c9583a17b24e3d9c15ad686667a1c5036cf7520469ad3e5cb1e57f1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+SampleAppCh3::Application.config.session_store :cookie_store, :key => '_sample_app_ch3_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# SampleAppCh3::Application.config.session_store :active_record_store
